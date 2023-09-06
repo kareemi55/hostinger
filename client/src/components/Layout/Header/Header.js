088a7e363e3507949586fef91e3736dfe1526f06
abid karim <110599002+kareemi55@users.noreply.github.com>
2023-09-06 19:17:39 +0500

    Update Header.js

--- client/src/components/Layout/Header/Header.js
+++ client/src/components/Layout/Header/Header.js
@@ -56,7 +56,7 @@ const Header = () => {
                       fontWeight: "700",
                       color: "#f3f3f3",
                     }}>
-                    IMAN TILES test
+                    IMAN TILES private limited 
                   </span>
                 </div>
               </Link>
